d468ed5a86a5b155965a7439d2631767e4b3c6ef
pc <pc@192.168.1.53>
2024-02-28 12:24:46 +0530

    commiting the branch

--- GITProj01/src/main/java/com/nt/payment/PaymentOperationsService.java
+++ GITProj01/src/main/java/com/nt/payment/PaymentOperationsService.java
@@ -9,6 +9,7 @@ public class PaymentOperationsService {
      {
     	 return "debit card payment is done";
      }
+     
      public String doUPIPayment()
      {
     	 return "UPI payment is done";
